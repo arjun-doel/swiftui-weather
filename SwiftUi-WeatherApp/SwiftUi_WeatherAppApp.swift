dfa8a5e45189bffcfcc1eafed5d5c71f101aab21
Arjun <arjundoel@gmail.com>
2022-01-13 20:03:13 +0000

    Initial Commit

---
+++ SwiftUi-WeatherApp/SwiftUi_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherAppApp.swift
+//  SwiftUi-WeatherApp
+//
+//  Created by Arjun Doel on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
