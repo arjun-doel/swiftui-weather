154e34c838ee0e8b0a203a73c20ab4e1837a14df
Arjun <arjundoel@gmail.com>
2022-01-13 21:37:25 +0000

    refactor...

--- SwiftUi-WeatherApp/ContentView.swift
+++ SwiftUi-WeatherApp/ContentView.swift
@@ -12,22 +12,9 @@ struct ContentView: View {
         ZStack {
             BackgroundView(topColor: .red, middleColor1: .blue, middleColor2: .white, bottomColor: .green)
             VStack {
-                Text("Cuppertino, CA")
-                    .font(.system(size: 29, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
+                CityTextView(cityName: "Cuppertino, CA")
                 
-                VStack(spacing: 8) {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    
-                    Text("25℃")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-                }
+                MainWeatherView(weatherImage: "cloud.sun.fill", temperature: 25)
                 
                 HStack(spacing: 15) {
                     WeatherDayView(dayOfWeek: "MON", imageName: "sun.max.fill", temperature: 30)
@@ -101,5 +88,35 @@ struct BackgroundView: View {
                        endPoint: .bottomTrailing)
             .blur(radius: 100, opaque: true)
             .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 29, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherView: View {
+    var weatherImage: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: weatherImage)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)℃")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
